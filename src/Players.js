802dddb55881b608f2360b9f26d5b30a4c798965
Sevilka77 <sevil-perm@mail.ru>
2022-04-08 14:44:38 +0500

    111

--- src/Players.js
+++ src/Players.js
@@ -4,24 +4,30 @@ import './styles/radial.css'
 // row of players
 const Players = (props) => {
   const { ctx, playerID } = props
+
   const players = []
   for (let index = 0; index < ctx.numPlayers; index++) {
     const i = parseInt(ctx.playOrder[index])
     players.push(
       <li
         key={props.G.players[i].id + props.G.players[i].name}
-        className="player-container radialList clock radius5 f2"
+        className="player-container"
       >
-        {props.G.players[i].id}
+        <span className="displace radius15 ">
+          Игрок {props.G.players[i].id}
+        </span>
       </li>
     )
   }
+  for (let i = 0; i < parseInt(playerID); i++) {
+    players.push(players.shift())
+  }
 
   return (
-    <div className="battleground players radial list">
-      <div class="radialPivot">...</div>
+    <div className="battleground players radial list radius10">
+      <div className="radialPivot">...</div>
       <ul
-        class="radialList clock radius2 f2"
+        className="radialList clock  f2 "
         role="navigation"
         aria-label="menu items"
       >
