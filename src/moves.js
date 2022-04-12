e05e4160731b20a2cf4492c655cf056660b30817
Sevilka77 <sevil-perm@mail.ru>
2022-04-12 15:52:27 +0500

    Заглушки для действий карт

--- src/moves.js
+++ src/moves.js
@@ -12,3 +12,40 @@ export const DrawCard = (G, ctx) => {
   const card = G.deckOnBoard.pop()
   G.players[ctx.currentPlayer].hand.push(card)
 }
+
+export const Analysis = (G, ctx) => {
+  //Посмотреть карты на руке соседа
+}
+export const Axe = (G, ctx) => {
+  //Убрать карантин на себе или соседнем игроке, или убрать дверь между
+}
+
+export const BarredDoor = (G, ctx) => {
+  //Установить дверь слеа или справа
+}
+
+export const SwapCardOther = (G, ctx) => {
+  // Поменяться одной картой с любым игрокм
+}
+
+export const ShowCardsSosedu = (G, ctx) => {
+  // Показать карты соседу
+}
+export const SwapCardDeck = (G, ctx) => {
+  // Поменять карту с руки на верхнюю колоды.Ход заканчивается
+}
+
+export const ChangePlaces = (G, ctx) => {
+  // Поменяться с соседним игроком местами, проверка на дверь и карантин
+}
+
+export const FlameThrower = (G, ctx) => {
+  // Убить игрока
+}
+
+export const Forgetfull = (G, ctx) => {
+  // Сбросить 3 карты и взять 3 новые.
+}
+export const ChangePlacesOther = (G, ctx) => {
+  // Поменяться с любым игроком если он не на карантине
+}
